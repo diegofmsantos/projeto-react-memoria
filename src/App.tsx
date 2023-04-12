34891ea707fe0451ca2f68aca0696017c941e625
Diego Santos <diegofmsantos0@gmail.com>
2023-04-11 21:00:26 -0300

    Criando Grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import * as C from "./App.styles";
+import logoImage from "./assets/devmemory_logo.png"
+import RestartIcon from "./svgs/restart.svg"
+import { InfoItem } from "./components/InfoItem/InfoItem";
+import { Button } from "./components/Button/Button";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const [playing, setPlaying] = useState<boolean>(false)
+  const [timeElapsed, setTimeElapsed] = useState<number>(0)
+  const [moveCount, setMoveCount] = useState<number>(0)
+
+
+  useEffect(() => resetAndCreateGrid(), [])
+
+  const resetAndCreateGrid = () => {
+
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <C.Container>
+        <C.Info>
+
+          <C.LogoLink href="">
+            <img src={logoImage} width={200} alt="" />
+          </C.LogoLink>
+
+          <C.InfoArea>
+            <InfoItem label="Tempo" value="00:00"></InfoItem>
+            <InfoItem label="Movimentos" value="0"></InfoItem>
+          </C.InfoArea>
+
+          <Button label="Reiniciar" icon={RestartIcon} onClick={resetAndCreateGrid} />
+        </C.Info>
+
+        <C.GridArea>
+          <C.Grid>
+
+          </C.Grid>
+        </C.GridArea>
+      </C.Container>
     </div>
   )
 }
