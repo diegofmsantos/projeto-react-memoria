89cbd722a49195c87f06d6f2f75777de0b89dc15
Diego Santos <diegofmsantos0@gmail.com>
2023-04-12 15:32:31 -0300

    exibindo grid 2

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ const App = () => {
   }
 
   const handleItemClick = (index: number) => {
-
+    
   }
 
   return (
--- src/components/GridItem/GridItem.tsx
+++ src/components/GridItem/GridItem.tsx
@@ -1,5 +1,7 @@
 import { GridItemType } from "../../types/GridItemTypes";
 import * as C from "./style";
+import b7Svg from "../../svgs/b7.svg"
+import { items } from "../../data/items";
 
 type Props = {
     item: GridItemType,
@@ -9,7 +11,8 @@ type Props = {
 export const GridItem = ({item, onClick}: Props) => {
     return (
         <C.Container onClick={onClick}>
-            ...
+            {!item.permanentShown && !item.shown && <C.Icon src={b7Svg} />}
+            {(item.permanentShown || item.shown) && item.item !== null && <C.Icon src={items[item.item].icon} />}
         </C.Container>
     )
 }
